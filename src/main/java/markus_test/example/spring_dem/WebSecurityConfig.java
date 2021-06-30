f3f742faf5fdfaaa9f7579ed61a8048d788e94ae
Markus <m.gamperle@gmail.com>
2021-06-30 20:33:42 +0200

    test

--- src/main/java/markus_test/example/spring_dem/WebSecurityConfig.java
+++ src/main/java/markus_test/example/spring_dem/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
    @Override
    protected void configure(HttpSecurity http) throws Exception {
+	   
       http
          .authorizeRequests()
             .mvcMatchers("/article", "/about", "/home").permitAll()
